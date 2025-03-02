9ab923f0ea1ce5c2a714a72ecd6dccb121f03a7c
Askh <149861091+Xaxathon@users.noreply.github.com>
2025-03-02 15:22:14 +0300

    update

--- client/src/pages/NFTSearchPage.tsx
+++ client/src/pages/NFTSearchPage.tsx
@@ -9,55 +9,28 @@ import { selectIsPreviewMode } from "../store/slices/nftVisualizerSlice";
 const NFTSearchPage = () => {
 	const isPreviewMode = useAppSelector(selectIsPreviewMode);
 	const [isMobile, setIsMobile] = useState(false);
-	const [viewportHeight, setViewportHeight] = useState(0);
 
 	useEffect(() => {
 		const handleResize = () => {
 			setIsMobile(window.innerWidth < 768);
-			setViewportHeight(window.innerHeight);
 		};
 
 		handleResize();
 
 		window.addEventListener("resize", handleResize);
-		window.addEventListener("scroll", handleResize);
-
-		return () => {
-			window.removeEventListener("resize", handleResize);
-			window.removeEventListener("scroll", handleResize);
-		};
+		return () => window.removeEventListener("resize", handleResize);
 	}, []);
 
 	return (
-		<div className="bg-gray-900 text-white p-4 sm:p-6 min-h-screen">
+		<div className="bg-gray-900 text-white p-4 sm:p-6">
 			<div className="max-w-7xl mx-auto">
 				{isMobile ? (
-					<div
-						className="grid grid-cols-1 bg-gray-800/20 p-3 border border-gray-700/50 backdrop-blur-sm overflow-hidden rounded-2xl shadow-lg gap-2 mb-4"
-						style={{
-							maxHeight: viewportHeight ? `${viewportHeight - 32}px` : "auto",
-						}}
-					>
-						<div
-							className="bg-gray-800/20 backdrop-blur-sm rounded-3xl overflow-hidden shadow-lg border border-gray-700/50"
-							style={{
-								height: "50vw", // Половина ширины экрана вместо aspect-square
-								maxHeight: viewportHeight
-									? `${(viewportHeight - 32) / 2.2}px`
-									: "40vh",
-							}}
-						>
+					<div className="grid grid-cols-1 bg-gray-800/20 p-4 border border-gray-700/50 backdrop-blur-sm overflow-hidden rounded-2xl shadow-lg gap-2 mb-4">
+						<div className="bg-gray-800/20 backdrop-blur-sm rounded-3xl  overflow-hidden shadow-lg border border-gray-700/50 aspect-square">
 							<GiftVisualizer />
 						</div>
 
-						<div
-							className="rounded-3xl border-gray-700/50 overflow-auto"
-							style={{
-								maxHeight: viewportHeight
-									? `${(viewportHeight - 32) / 2}px`
-									: "45vh",
-							}}
-						>
+						<div className="rounded-3xl border-gray-700/50 md:aspect-square">
 							<SearchForm />
 						</div>
 					</div>
